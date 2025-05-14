9c16a9bfc536afc5e0b967b09f71a40ffca7dcc2
ruicalvario77 <ruic7777@gmail.com>
2025-05-14 11:45:36 +0000

    Another Test

--- wp-content/plugins/just-beautiful-customizer/just-beautiful-customizer.php
+++ wp-content/plugins/just-beautiful-customizer/just-beautiful-customizer.php
@@ -499,6 +499,13 @@ function jbc_customization_tab_content() {
     $allowed_zones = get_post_meta($product_id, '_jbc_allowed_zones', true) ?: [];
 
     ?>
+    <!-- Inline CSS to prevent overlapping -->
+    <style>
+        .jbc-placement-zones {
+            margin-left: 20px; /* Indents the checkboxes */
+        }
+    </style>
+
     <div id="jbc_customization_data" class="panel woocommerce_options_panel">
         <div class="options_group">
             <p class="form-field">
@@ -508,12 +515,14 @@ function jbc_customization_tab_content() {
             <?php if (!empty($category_zones)) : ?>
                 <p class="form-field">
                     <label><?php _e('Allowed Placement Zones', 'just-beautiful-customizer'); ?></label>
-                    <?php foreach ($category_zones as $index => $zone) : ?>
-                        <label>
-                            <input type="checkbox" name="jbc_allowed_zones[]" value="<?php echo $index; ?>" <?php checked(in_array($index, $allowed_zones)); ?>>
-                            <?php echo esc_html($zone['name']); ?>
-                        </label><br>
-                    <?php endforeach; ?>
+                    <div class="jbc-placement-zones">
+                        <?php foreach ($category_zones as $index => $zone) : ?>
+                            <label>
+                                <input type="checkbox" name="jbc_allowed_zones[]" value="<?php echo $index; ?>" <?php checked(in_array($index, $allowed_zones)); ?>>
+                                <?php echo esc_html($zone['name']); ?>
+                            </label><br>
+                        <?php endforeach; ?>
+                    </div>
                 </p>
             <?php else : ?>
                 <p><?php _e('No placement zones defined for this category.', 'just-beautiful-customizer'); ?></p>
